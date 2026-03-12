d6e1244c34e1149251de7b583e3e9998a150c188
Chathura Dharmasiri <chathuramaheshdharmasiride@gmail.com>
2026-03-12 19:45:02 +0530

    Day 3 | 1 digit operations

--- Day 2/opera.asm
+++ Day 2/opera.asm
@@ -2,72 +2,63 @@
 .stack 100h
 
 .data
-n1 db 4
-n2 db 2
-
-msg1 db "Addition is : $"
-msg2 db 13,10,"Subtraction is : $"
-msg3 db 13,10,"Multiplication is : $"
-msg4 db 13,10,"Division is : $"
+msg1 db "Enter the number: $"
+t db 0 ;2
+o db 0 ;3
+dig db 0
+r db 0
 
 .code
-start:
-    mov ax, @data
-    mov ds, ax
+    mov ax,@data
+    mov ds,ax
 
-; -------- Addition --------
-    mov ah, 09h
-    lea dx, msg1
-    int 21h
-
-    mov al, n1
-    add al, n2
-    add al, 48
-    mov dl, al
-    mov ah, 02h
-    int 21h
-
-; -------- Subtraction --------
-    mov ah, 09h
-    lea dx, msg2
-    int 21h
-
-    mov al, n1
-    sub al, n2
-    add al, 48
-    mov dl, al
-    mov ah, 02h
-    int 21h
-
-; -------- Multiplication --------
-    mov ah, 09h
-    lea dx, msg3
-    int 21h
-
-    mov al, n1
-    mov bl, n2
-    mul bl
-    add al, 48
-    mov dl, al
-    mov ah, 02h
-    int 21h
-
-; -------- Division --------
-    mov ah, 09h
-    lea dx, msg4
-    int 21h
-
-    mov al, n1
-    cbw
-    mov bl, n2
-    div bl
-    add al, 48
-    mov dl, al
-    mov ah, 02h
-    int 21h
-
-; Exit
-    mov ah, 4Ch
-    int 21h
-
-end start
+        mov dx,offset msg1
+        mov ah,09h
+        int 21h
+        
+    ;read the first digit
+        mov ah,01h
+        int 21h
+        sub al,48
+        mov t,al
+        
+        
+    ;read the second digit
+        mov ah,01h
+        int 21h
+        sub al,48
+        mov o,al
+        
+        mov dl,10
+        mov ah,02h
+        int 21h
+        
+    ;unknown variable
+        mov bl,10
+        
+        mov al,t
+        mul bl
+        add al,o
+        
+        mov dig,al  ;23
+        
+        mov al,dig
+        mov ah,00
+        div bl
+        
+        mov r,ah
+        
+        mov dl,al
+        add dl,48
+        mov ah,02h
+        int 21h
+        
+        mov dl,r
+        add dl,48
+        mov ah,02h
+        int 21h
+        
+        mov ax,4c00h
+        int 21h
+        
+end
--- Day 3/Operations.asm
+++ Day 3/Operations.asm
@@ -1,1 +1,106 @@
-no..
+.model small
+.stack 100h
+
+.data
+msg1 db "Enter first number (0-9): $"
+msg2 db 13,10,"Enter operator (+,-,*,/): $"
+msg3 db 13,10,"Enter second number (0-9): $"
+msg4 db 13,10,"Result is: $"
+
+n1 db ?
+n2 db ?
+op db ?
+
+.code
+start:
+    mov ax,@data
+    mov ds,ax
+
+; -------- Input First Number --------
+    mov ah,09h
+    lea dx,msg1
+    int 21h
+
+    mov ah,01h
+    int 21h
+    sub al,48
+    mov n1,al
+
+; -------- Input Operator --------
+    mov ah,09h
+    lea dx,msg2
+    int 21h
+
+    mov ah,01h
+    int 21h
+    mov op,al
+
+; -------- Input Second Number --------
+    mov ah,09h
+    lea dx,msg3
+    int 21h
+
+    mov ah,01h
+    int 21h
+    sub al,48
+    mov n2,al
+
+; -------- Print Result Message --------
+    mov ah,09h
+    lea dx,msg4
+    int 21h
+
+; -------- Check Operator --------
+    mov al,op
+
+    cmp al,'+'
+    je ADDITION
+
+    cmp al,'-'
+    je SUBTRACTION
+
+    cmp al,'*'
+    je MULTIPLICATION
+
+    cmp al,'/'
+    je DIVISION
+
+; -------- Addition --------
+ADDITION:
+    mov al,n1
+    add al,n2
+    jmp PRINT
+
+; -------- Subtraction --------
+SUBTRACTION:
+    mov al,n1
+    sub al,n2
+    jmp PRINT
+
+; -------- Multiplication --------
+MULTIPLICATION:
+    mov al,n1
+    mov bl,n2
+    mul bl
+    jmp PRINT
+
+; -------- Division --------
+DIVISION:
+    mov al,n1
+    mov ah,0
+    mov bl,n2
+    div bl
+    jmp PRINT
+
+; -------- Print Result --------
+PRINT:
+    add al,48
+    mov dl,al
+    mov ah,02h
+    int 21h
+
+; -------- Exit --------
+    mov ah,4Ch
+    int 21h
+
+end start
